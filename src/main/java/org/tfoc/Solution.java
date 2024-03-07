a55b1e8720479336db9e826b76dd2041acc2303a
Jesús Peiró López <jpeirolo@mercadona.es>
2024-03-07 11:43:35 +0100

    feat: Solution with tests

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,33 @@
 package org.tfoc;
 
+import java.util.Arrays;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public boolean isAnagram(
+    public static boolean isAnagram(
             String s,
             String t) {
 
-        return false;
+        if(s != null && t != null && t.length() == s.length()){
+
+            s = s.toLowerCase();
+            t = t.toLowerCase();
+
+            var sCharArray = s.toLowerCase().toCharArray();
+            var tCharArray = t.toLowerCase().toCharArray();
+
+
+            Arrays.sort(sCharArray);
+            Arrays.sort(tCharArray);
+
+            return Arrays.equals(sCharArray,tCharArray);
+
+        } else {
+            return false;
+        }
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,66 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Slf4j
 class SolutionTest {
 
     @Test
     void testSolution() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        String word = "anagram";
+        String possibleAnagram = "nagaram";
+        boolean result = Solution.isAnagram(word, possibleAnagram);
+
+        assertTrue(result);
+
     }
+
+    @Test
+    void testSolutionFailed() {
+
+        String word = "rat";
+        String possibleAnagram = "car";
+        boolean result = Solution.isAnagram(word, possibleAnagram);
+
+        assertFalse(result);
+
+    }
+
+    @Test
+    void testLowerCase() {
+
+        String word = "ANAGRAM";
+        String possibleAnagram = "nagaram";
+        boolean result = Solution.isAnagram(word, possibleAnagram);
+
+        assertTrue(result);
+
+    }
+
+    @Test
+    void testNull() {
+
+        String possibleAnagram = "car";
+        boolean result = Solution.isAnagram(null, possibleAnagram);
+
+        assertFalse(result);
+
+    }
+
+    @Test
+    void testNotEqualLength() {
+
+        String word = "carrrr";
+        String possibleAnagram = "car";
+        boolean result = Solution.isAnagram(word, possibleAnagram);
+
+        assertFalse(result);
+
+    }
+
+
 
 }
